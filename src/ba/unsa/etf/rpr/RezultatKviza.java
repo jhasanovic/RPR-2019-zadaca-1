04b1d1a665b69a0b8d572b499c3a0069477b1e03
jhasanovic <jhasanovic1@etf.unsa.ba>
2020-11-08 19:08:47 +0100

    Implementirana metoda toString u klasi RezultatKviza

--- src/ba/unsa/etf/rpr/RezultatKviza.java
+++ src/ba/unsa/etf/rpr/RezultatKviza.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 public class RezultatKviza {
@@ -32,4 +33,14 @@ public class RezultatKviza {
             return bodovi;
     }
 
+    @Override
+    public String toString() {
+        String s=String.format("Na kvizu "+kviz.getNaziv()+ " ostvarili ste ukupno %.1f poena. Raspored po pitanjima:\n",getTotal());
+        for (Map.Entry<Pitanje, Double> m : bodovi.entrySet()) {
+            s=s+m.getKey();
+            s=s+String.format(" - %.1f",m.getValue());
+            s=s+"b\n";
+        }
+        return s;
+    }
 };
